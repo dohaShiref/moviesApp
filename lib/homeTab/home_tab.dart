6a9991d81ba5bf4c8bec1c333744c1145c3d73ce
dohaShiref <50174692+dohaShiref@users.noreply.github.com>
2021-12-09 08:54:15 +0200

    Home Screen

---
+++ lib/homeTab/home_tab.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class HomeTab extends StatelessWidget{
+  static const String routeName = 'home';
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return Container(
+
+    );
+  }
+
+}
---
+++ lib/home_screen.dart
@@ -0,0 +1,92 @@
+
+import 'package:flutter/material.dart';
+
+import 'BrowseTab/browse_tab.dart';
+import 'SearchTab/search_tab.dart';
+import 'WatchListTab/watch_list_tab.dart';
+import 'homeTab/home_tab.dart';
+import 'main.dart';
+
+class homeScreen extends StatefulWidget {
+  static const String routeName = 'home';
+
+  @override
+  State<homeScreen> createState() => _homeScreenState();
+}
+
+class _homeScreenState extends State<homeScreen> {
+  int currentPageIndex = 0;
+
+  @override
+  Widget build(BuildContext context) {
+
+    return Stack(
+      children: [
+        Scaffold(
+          backgroundColor: Colors.black,
+          bottomNavigationBar: Theme(
+            data: Theme.of(context)
+                .copyWith(canvasColor: MyThemeData.BotomBarColor),
+            child: BottomNavigationBar(
+              onTap: (index) {
+                currentPageIndex = index;
+                setState(() {});
+              },
+              currentIndex: currentPageIndex,
+              selectedItemColor: MyThemeData.Primaty_color,
+              unselectedItemColor: MyThemeData.UnSelectedIemColor,
+              selectedIconTheme:
+              IconThemeData(color: MyThemeData.Primaty_color),
+              unselectedIconTheme:
+              IconThemeData(color: MyThemeData.UnSelectedIemColor),
+              items: [
+                BottomNavigationBarItem(
+                    icon: Image.asset('assets/homeIcon.png',
+                        width: 37,
+                        color: currentPageIndex == 0
+                            ? MyThemeData.Primaty_color
+                            : MyThemeData.UnSelectedIemColor),
+                    label:'Home'),
+                BottomNavigationBarItem(
+                    label: 'Search',
+                    icon: Image.asset('assets/search.png',
+                        width: 37,
+                        color: currentPageIndex == 1
+                            ? MyThemeData.Primaty_color
+                            : MyThemeData.UnSelectedIemColor)),
+                BottomNavigationBarItem(
+                    label: 'browse',
+                    icon: Image.asset('assets/browse.png',
+                        width: 37,
+                        color: currentPageIndex == 2
+                            ? MyThemeData.Primaty_color
+                            : MyThemeData.UnSelectedIemColor)),
+                BottomNavigationBarItem(
+                    label: 'WatchList',
+                    icon: Image.asset('assets/watchList.png',
+                        width: 37,
+                        color: currentPageIndex == 3
+                            ? MyThemeData.Primaty_color
+                            : MyThemeData.UnSelectedIemColor)),
+              ],
+            ),
+          ),
+          body: getCurrentPage(),
+        ),
+      ],
+    );
+  }
+
+  Widget getCurrentPage() {
+    if (currentPageIndex == 0) {
+      return HomeTab();
+    } else if (currentPageIndex == 1) {
+      return SearchTab();
+    } else if (currentPageIndex == 2) {
+      return BrowseTab();
+    }
+    else{
+      return WatchListTab();
+    }
+  }
+}
