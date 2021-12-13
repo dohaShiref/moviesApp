5f4b3fad84bb7385c242115229d96c0ed910dea6
Aml Ahmed <61411988+AmlAhmed99@users.noreply.github.com>
2021-12-13 17:22:00 +0200

    api

---
+++ lib/models/related_response.dart
@@ -0,0 +1,164 @@
+/// page : 1
+/// results : [{"adult":false,"backdrop_path":"/5hNcsnMkwU2LknLoru73c76el3z.jpg","genre_ids":[35,18,10749],"id":19404,"original_language":"hi","original_title":"दिलवाले दुल्हनिया ले जायेंगे","overview":"Raj is a rich, carefree, happy-go-lucky second generation NRI. Simran is the daughter of Chaudhary Baldev Singh, who in spite of being an NRI is very strict about adherence to Indian values. Simran has left for India to be married to her childhood fiancé. Raj leaves for India with a mission at his hands, to claim his lady love under the noses of her whole family. Thus begins a saga.","popularity":28.053,"poster_path":"/2CAL2433ZeIihfX1Hb2139CX0pW.jpg","release_date":"1995-10-20","title":"Dilwale Dulhania Le Jayenge","video":false,"vote_average":8.7,"vote_count":3268},{"adult":false,"backdrop_path":"/iNh3BivHyg5sQRPP1KOkzguEX0H.jpg","genre_ids":[18,80],"id":278,"original_language":"en","original_title":"The Shawshank Redemption","overview":"Framed in the 1940s for the double murder of his wife and her lover, upstanding banker Andy Dufresne begins a new life at the Shawshank prison, where he puts his accounting skills to work for an amoral warden. During his long stretch in prison, Dufresne comes to be admired by the other inmates -- including an older prisoner named Red -- for his integrity and unquenchable sense of hope.","popularity":75.426,"poster_path":"/q6y0Go1tsGEsmtFryDOJo3dEmqu.jpg","release_date":"1994-09-23","title":"The Shawshank Redemption","video":false,"vote_average":8.7,"vote_count":20265},{"adult":false,"backdrop_path":"/rSPw7tgCH9c6NqICZef4kZjFOQ5.jpg","genre_ids":[18,80],"id":238,"original_language":"en","original_title":"The Godfather","overview":"Spanning the years 1945 to 1955, a chronicle of the fictional Italian-American Corleone crime family. When organized crime family patriarch, Vito Corleone barely survives an attempt on his life, his youngest son, Michael steps in to take care of the would-be killers, launching a campaign of bloody revenge.","popularity":69.183,"poster_path":"/eEslKSwcqmiNS6va24Pbxf2UKmJ.jpg","release_date":"1972-03-14","title":"The Godfather","video":false,"vote_average":8.7,"vote_count":15165},{"adult":false,"backdrop_path":"/jtAI6OJIWLWiRItNSZoWjrsUtmi.jpg","genre_ids":[10749],"id":724089,"original_language":"en","original_title":"Gabriel's Inferno Part II","overview":"Professor Gabriel Emerson finally learns the truth about Julia Mitchell's identity, but his realization comes a moment too late. Julia is done waiting for the well-respected Dante specialist to remember her and wants nothing more to do with him. Can Gabriel win back her heart before she finds love in another's arms?","popularity":17.573,"poster_path":"/x5o8cLZfEXMoZczTYWLrUo1P7UJ.jpg","release_date":"2020-07-31","title":"Gabriel's Inferno Part II","video":false,"vote_average":8.7,"vote_count":1349},{"adult":false,"backdrop_path":"/fQq1FWp1rC89xDrRMuyFJdFUdMd.jpg","genre_ids":[10749,35],"id":761053,"original_language":"en","original_title":"Gabriel's Inferno Part III","overview":"The final part of the film adaption of the erotic romance novel Gabriel's Inferno written by an anonymous Canadian author under the pen name Sylvain Reynard.","popularity":31.323,"poster_path":"/qtX2Fg9MTmrbgN1UUvGoCsImTM8.jpg","release_date":"2020-11-19","title":"Gabriel's Inferno Part III","video":false,"vote_average":8.6,"vote_count":909},{"adult":false,"backdrop_path":"/loRmRzQXZeqG78TqZuyvSlEQfZb.jpg","genre_ids":[18,36,10752],"id":424,"original_language":"en","original_title":"Schindler's List","overview":"The true story of how businessman Oskar Schindler saved over a thousand Jewish lives from the Nazis while they worked as slaves in his factory during World War II.","popularity":39.397,"poster_path":"/sF1U4EUQS8YHUYjNl3pMGNIQyr0.jpg","release_date":"1993-11-30","title":"Schindler's List","video":false,"vote_average":8.6,"vote_count":12111},{"adult":false,"backdrop_path":"/w2uGvCpMtvRqZg6waC1hvLyZoJa.jpg","genre_ids":[10749],"id":696374,"original_language":"en","original_title":"Gabriel's Inferno","overview":"An intriguing and sinful exploration of seduction, forbidden love, and redemption, Gabriel's Inferno is a captivating and wildly passionate tale of one man's escape from his own personal hell as he tries to earn the impossible--forgiveness and love.","popularity":14.102,"poster_path":"/oyG9TL7FcRP4EZ9Vid6uKzwdndz.jpg","release_date":"2020-05-29","title":"Gabriel's Inferno","video":false,"vote_average":8.6,"vote_count":2166},{"adult":false,"backdrop_path":"/poec6RqOKY9iSiIUmfyfPfiLtvB.jpg","genre_ids":[18,80],"id":240,"original_language":"en","original_title":"The Godfather: Part II","overview":"In the continuing saga of the Corleone crime family, a young Vito Corleone grows up in Sicily and in 1910s New York. In the 1950s, Michael Corleone attempts to expand the family business into Las Vegas, Hollywood and Cuba.","popularity":47.038,"poster_path":"/sSuQTCZwqKrNBNIsksO9IAUoWP9.jpg","release_date":"1974-12-20","title":"The Godfather: Part II","video":false,"vote_average":8.6,"vote_count":9129},{"adult":false,"backdrop_path":"/l5K9elugftlcyIHHm4nylvsn26X.jpg","genre_ids":[18],"id":255709,"original_language":"ko","original_title":"소원","overview":"After 8-year-old So-won narrowly survives a brutal sexual assault, her family labors to help her heal while coping with their own rage and grief.","popularity":13.346,"poster_path":"/x9yjkm9gIz5qI5fJMUTfBnWiB2o.jpg","release_date":"2013-10-02","title":"Hope","video":false,"vote_average":8.6,"vote_count":236},{"adult":false,"backdrop_path":"/1EAxNqdkVnp48a7NUuNBHGflowM.jpg","genre_ids":[16,28,878],"id":283566,"original_language":"ja","original_title":"シン・エヴァンゲリオン劇場版:||","overview":"In the aftermath of the Fourth Impact, stranded without their Evangelions, Shinji, Asuka, and Rei find refuge in one of the rare pockets of humanity that still exist on the ruined planet Earth. There, each of them live a life far different from their days as an Evangelion pilot. However, the danger to the world is far from over. A new impact is looming on the horizon—one that will prove to be the true end of Evangelion.","popularity":175.395,"poster_path":"/g7xEBLb5hOTPpjQxIMrCNWbnjna.jpg","release_date":"2021-03-08","title":"Evangelion: 3.0+1.0 Thrice Upon a Time","video":false,"vote_average":8.6,"vote_count":392},{"adult":false,"backdrop_path":"/mMtUybQ6hL24FXo0F3Z4j2KG7kZ.jpg","genre_ids":[10749,16,18],"id":372058,"original_language":"ja","original_title":"君の名は。","overview":"High schoolers Mitsuha and Taki are complete strangers living separate lives. But one night, they suddenly switch places. Mitsuha wakes up in Taki’s body, and he in hers. This bizarre occurrence continues to happen randomly, and the two must adjust their lives around each other.","popularity":167.013,"poster_path":"/q719jXXEzOoYaps6babgKnONONX.jpg","release_date":"2016-08-26","title":"Your Name.","video":false,"vote_average":8.6,"vote_count":8126},{"adult":false,"backdrop_path":"/Ab8mkHmkYADjU7wQiOkia9BzGvS.jpg","genre_ids":[16,10751,14],"id":129,"original_language":"ja","original_title":"千と千尋の神隠し","overview":"A young girl, Chihiro, becomes trapped in a strange new world of spirits. When her parents undergo a mysterious transformation, she must call upon the courage she never knew she had to free her family.","popularity":78.768,"poster_path":"/39wmItIWsg5sZMyRUHLkWBcuVCM.jpg","release_date":"2001-07-20","title":"Spirited Away","video":false,"vote_average":8.5,"vote_count":12136},{"adult":false,"backdrop_path":"/TU9NIjwzjoKPwQHoHshkFcQUCG.jpg","genre_ids":[35,53,18],"id":496243,"original_language":"ko","original_title":"기생충","overview":"All unemployed, Ki-taek's family takes peculiar interest in the wealthy and glamorous Parks for their livelihood until they get entangled in an unexpected incident.","popularity":104.231,"poster_path":"/7IiTTgloJzvGI1TAYymCfbfl3vT.jpg","release_date":"2019-05-30","title":"Parasite","video":false,"vote_average":8.5,"vote_count":12654},{"adult":false,"backdrop_path":"/u1wHUA0R48FH4WV3sGqjwx3aNZm.jpg","genre_ids":[16,18,10402,10749],"id":632632,"original_language":"ja","original_title":"映画 ギヴン","overview":"The film centers on the love relationship among the band's bassist Haruki Nakayama, drummer Akihiko Kaji, and Akihiko's roommate and ex-boyfriend Ugetsu Murata.","popularity":122.705,"poster_path":"/xKCtoYHUyX8zAg68eemnYa2orep.jpg","release_date":"2020-08-22","title":"Given","video":false,"vote_average":8.5,"vote_count":275},{"adult":false,"backdrop_path":"/iUeeZ5PWfZGgUtCJfwcgmCfdzoI.jpg","genre_ids":[16,878,10751,35],"id":482321,"original_language":"en","original_title":"Ron's Gone Wrong","overview":"In a world where walking, talking, digitally connected bots have become children's best friends, an 11-year-old finds that his robot buddy doesn't quite work the same as the others do.","popularity":2662.308,"poster_path":"/gA9QxSravC2EVEkEKgyEmDrfL0e.jpg","release_date":"2021-10-15","title":"Ron's Gone Wrong","video":false,"vote_average":8.5,"vote_count":320},{"adult":false,"backdrop_path":"/qqHQsStV6exghCM7zbObuYBiYxw.jpg","genre_ids":[18],"id":389,"original_language":"en","original_title":"12 Angry Men","overview":"The defense and the prosecution have rested and the jury is filing into the jury room to decide if a young Spanish-American is guilty or innocent of murdering his father. What begins as an open and shut case soon becomes a mini-drama of each of the jurors' prejudices and preconceptions about the trial, the accused, and each other.","popularity":28.178,"poster_path":"/e02s4wmTAExkKg0yF4dEG98ZRpK.jpg","release_date":"1957-04-10","title":"12 Angry Men","video":false,"vote_average":8.5,"vote_count":6010},{"adult":false,"backdrop_path":"/l6hQWH9eDksNJNiXWYRkWqikOdu.jpg","genre_ids":[14,18,80],"id":497,"original_language":"en","original_title":"The Green Mile","overview":"A supernatural tale set on death row in a Southern prison, where gentle giant John Coffey possesses the mysterious power to heal people's ailments. When the cell block's head guard, Paul Edgecomb, recognizes Coffey's miraculous gift, he tries desperately to help stave off the condemned man's execution.","popularity":62.121,"poster_path":"/velWPhVMQeQKcxggNEU8YmIo52R.jpg","release_date":"1999-12-10","title":"The Green Mile","video":false,"vote_average":8.5,"vote_count":13159},{"adult":false,"backdrop_path":"/1fOsyhVz5qyX2rl1qqX6KImVhTx.jpg","genre_ids":[18,35],"id":644479,"original_language":"es","original_title":"Dedicada a mi ex","overview":"The film tells the story of Ariel, a 21-year-old who decides to form a rock band to compete for a prize of ten thousand dollars in a musical band contest, this as a last option when trying to get money to save their relationship and reunite with his ex-girlfriend, which breaks due to the trip she must make to Finland for an internship. Ariel with her friend Ortega, decides to make a casting to find the other members of the band, although they do not know nothing about music, thus forming a band with members that have diverse and opposite personalities.","popularity":18.724,"poster_path":"/xc4bTXVwYNXi10jG9dwcaYt5IpU.jpg","release_date":"2019-11-01","title":"Dedicated to my ex","video":false,"vote_average":8.5,"vote_count":440},{"adult":false,"backdrop_path":"/m5HPKCi7GdhKmxPTcOQmcLfEmZ9.jpg","genre_ids":[12,35,16,14],"id":532067,"original_language":"ja","original_title":"映画 この素晴らしい世界に祝福を！紅伝説","overview":"It is not strange that the Demon Lord's forces fear the Crimson Demons, the clan from which Megumin and Yunyun originate. Even if the Demon Lord's generals attack their village, the Crimson Demons can just easily brush them off with their supreme mastery of advanced and overpowered magic.  When Yunyun receives a seemingly serious letter regarding a potential disaster coming to her hometown, she immediately informs Kazuma Satou and the rest of his party. After a series of wacky misunderstandings, it turns out to be a mere prank by her fellow demon who wants to be an author. Even so, Megumin becomes worried about her family and sets out toward the Crimson Demons' village with the gang.  There, Kazuma and the others decide to sightsee the wonders of Megumin's birthplace. However, they soon come to realize that the nonsense threat they received might have been more than just a joke.","popularity":182.159,"poster_path":"/j73LuQcA21KvkVFcroWWMN8tTJv.jpg","release_date":"2019-08-30","title":"KonoSuba: God's Blessing on this Wonderful World! Legend of Crimson","video":false,"vote_average":8.5,"vote_count":321},{"adult":false,"backdrop_path":"/yHtB4KHNigx3ZoxDvQbW2SOXGfq.jpg","genre_ids":[16,10751,12,14],"id":441130,"original_language":"en","original_title":"Wolfwalkers","overview":"In a time of superstition and magic, when wolves are seen as demonic and nature an evil to be tamed, a young apprentice hunter comes to Ireland with her father to wipe out the last pack. But when she saves a wild native girl, their friendship leads her to discover the world of the Wolfwalkers and transform her into the very thing her father is tasked to destroy.","popularity":20.917,"poster_path":"/ehAKuE48okTuonq6TpsNQj8vFTC.jpg","release_date":"2020-10-26","title":"Wolfwalkers","video":false,"vote_average":8.5,"vote_count":638}]
+/// total_pages : 471
+/// total_results : 9411
+
+class Related_response {
+  Related_response({
+      int page, 
+      List<Results> results, 
+      int totalPages, 
+      int totalResults,}){
+    _page = page;
+    _results = results;
+    _totalPages = totalPages;
+    _totalResults = totalResults;
+}
+
+  Related_response.fromJson(dynamic json) {
+    _page = json['page'];
+    if (json['results'] != null) {
+      _results = [];
+      json['results'].forEach((v) {
+        _results.add(Results.fromJson(v));
+      });
+    }
+    _totalPages = json['total_pages'];
+    _totalResults = json['total_results'];
+  }
+  int _page;
+  List<Results> _results;
+  int _totalPages;
+  int _totalResults;
+
+  int get page => _page;
+  List<Results> get results => _results;
+  int get totalPages => _totalPages;
+  int get totalResults => _totalResults;
+
+  Map<String, dynamic> toJson() {
+    final map = <String, dynamic>{};
+    map['page'] = _page;
+    if (_results != null) {
+      map['results'] = _results.map((v) => v.toJson()).toList();
+    }
+    map['total_pages'] = _totalPages;
+    map['total_results'] = _totalResults;
+    return map;
+  }
+
+}
+
+/// adult : false
+/// backdrop_path : "/5hNcsnMkwU2LknLoru73c76el3z.jpg"
+/// genre_ids : [35,18,10749]
+/// id : 19404
+/// original_language : "hi"
+/// original_title : "दिलवाले दुल्हनिया ले जायेंगे"
+/// overview : "Raj is a rich, carefree, happy-go-lucky second generation NRI. Simran is the daughter of Chaudhary Baldev Singh, who in spite of being an NRI is very strict about adherence to Indian values. Simran has left for India to be married to her childhood fiancé. Raj leaves for India with a mission at his hands, to claim his lady love under the noses of her whole family. Thus begins a saga."
+/// popularity : 28.053
+/// poster_path : "/2CAL2433ZeIihfX1Hb2139CX0pW.jpg"
+/// release_date : "1995-10-20"
+/// title : "Dilwale Dulhania Le Jayenge"
+/// video : false
+/// vote_average : 8.7
+/// vote_count : 3268
+
+class Results {
+  Results({
+      bool adult, 
+      String backdropPath, 
+      List<int> genreIds, 
+      int id, 
+      String originalLanguage, 
+      String originalTitle, 
+      String overview, 
+      double popularity, 
+      String posterPath, 
+      String releaseDate, 
+      String title, 
+      bool video, 
+      double voteAverage, 
+      int voteCount,}){
+    _adult = adult;
+    _backdropPath = backdropPath;
+    _genreIds = genreIds;
+    _id = id;
+    _originalLanguage = originalLanguage;
+    _originalTitle = originalTitle;
+    _overview = overview;
+    _popularity = popularity;
+    _posterPath = posterPath;
+    _releaseDate = releaseDate;
+    _title = title;
+    _video = video;
+    _voteAverage = voteAverage;
+    _voteCount = voteCount;
+}
+
+  Results.fromJson(dynamic json) {
+    _adult = json['adult'];
+    _backdropPath = json['backdrop_path'];
+    _genreIds = json['genre_ids'] != null ? json['genre_ids'].cast<int>() : [];
+    _id = json['id'];
+    _originalLanguage = json['original_language'];
+    _originalTitle = json['original_title'];
+    _overview = json['overview'];
+    _popularity = json['popularity'];
+    _posterPath = json['poster_path'];
+    _releaseDate = json['release_date'];
+    _title = json['title'];
+    _video = json['video'];
+    _voteAverage = json['vote_average'];
+    _voteCount = json['vote_count'];
+  }
+  bool _adult;
+  String _backdropPath;
+  List<int> _genreIds;
+  int _id;
+  String _originalLanguage;
+  String _originalTitle;
+  String _overview;
+  double _popularity;
+  String _posterPath;
+  String _releaseDate;
+  String _title;
+  bool _video;
+  double _voteAverage;
+  int _voteCount;
+
+  bool get adult => _adult;
+  String get backdropPath => _backdropPath;
+  List<int> get genreIds => _genreIds;
+  int get id => _id;
+  String get originalLanguage => _originalLanguage;
+  String get originalTitle => _originalTitle;
+  String get overview => _overview;
+  double get popularity => _popularity;
+  String get posterPath => _posterPath;
+  String get releaseDate => _releaseDate;
+  String get title => _title;
+  bool get video => _video;
+  double get voteAverage => _voteAverage;
+  int get voteCount => _voteCount;
+
+  Map<String, dynamic> toJson() {
+    final map = <String, dynamic>{};
+    map['adult'] = _adult;
+    map['backdrop_path'] = _backdropPath;
+    map['genre_ids'] = _genreIds;
+    map['id'] = _id;
+    map['original_language'] = _originalLanguage;
+    map['original_title'] = _originalTitle;
+    map['overview'] = _overview;
+    map['popularity'] = _popularity;
+    map['poster_path'] = _posterPath;
+    map['release_date'] = _releaseDate;
+    map['title'] = _title;
+    map['video'] = _video;
+    map['vote_average'] = _voteAverage;
+    map['vote_count'] = _voteCount;
+    return map;
+  }
+
+}
